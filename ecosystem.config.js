35b845759c85b67a75300d284ce09ac5585f242a
HarshK <harshkankaria9@gmail.com>
2026-01-29 17:32:28 +0530

    Refactor CI/CD to separate build and deploy jobs with standard Next.js production setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   apps: [{
     name: 'followupx_web',
-    script: 'node_modules/.bin/next',
-    args: 'start',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start -p 5001',
     env: {
       NODE_ENV: 'production',
-      NEXT_PUBLIC_API_BASE_URL: 'https://followapi.paperthoughts.in',
-      PORT: 5001
+      NEXT_PUBLIC_API_BASE_URL: 'https://followapi.paperthoughts.in'
     }
   }]
 };
